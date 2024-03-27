7457de4dbcc5565e691ca6ca5602ecbf73bcf4c0
Erick bonfati <erickbonfati39@gmail.com>
2024-03-27 14:14:11 -0300

    Adicionado cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import { resolve } from 'path';
 
-dotenv.config();
+// dotenv.config();
 
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import photoRoutes from './routes/photoRoutes';
+
+const whiteList = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -21,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images', express.static(resolve(__dirname, '..', 'uploads', 'images')));
