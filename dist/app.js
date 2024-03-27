c9ba77395679d4f213e314dafaf74e84b1a6db0a
Erick bonfati <erickbonfati39@gmail.com>
2024-03-27 14:15:19 -0300

    Adicionado cors

--- dist/app.js
+++ dist/app.js
@@ -1,40 +1,44 @@
-Object.defineProperty(exports, '__esModule', { value: true }); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } const _dotenv = require('dotenv');
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }// import dotenv from 'dotenv';
+var _path = require('path');
 
-const _dotenv2 = _interopRequireDefault(_dotenv);
-const _path = require('path');
-
-_dotenv2.default.config();
+// dotenv.config();
 
 require('./database');
 
-const _express = require('express');
+var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
-const _express2 = _interopRequireDefault(_express);
+var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
+var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
+var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
+var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
+var _photoRoutes = require('./routes/photoRoutes'); var _photoRoutes2 = _interopRequireDefault(_photoRoutes);
 
-const _homeRoutes = require('./routes/homeRoutes');
+const whiteList = [
+  'http://localhost:3000',
+];
 
-const _homeRoutes2 = _interopRequireDefault(_homeRoutes);
-const _userRoutes = require('./routes/userRoutes');
-
-const _userRoutes2 = _interopRequireDefault(_userRoutes);
-const _tokenRoutes = require('./routes/tokenRoutes');
-
-const _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
-const _alunoRoutes = require('./routes/alunoRoutes');
-
-const _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
-const _photoRoutes = require('./routes/photoRoutes');
-
-const _photoRoutes2 = _interopRequireDefault(_photoRoutes);
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
-    this.app = _express2.default.call(void 0);
+    this.app = _express2.default.call(void 0, );
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
@@ -49,4 +53,4 @@ class App {
   }
 }
 
-exports.default = new App().app; // Aqui estamos instanciando e exportando nossa classe
+exports. default = new App().app; // Aqui estamos instanciando e exportando nossa classe
